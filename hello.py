46c83a1856d11ccfb3fd703022392e6412d840c4
hirasaka-er <54878528+hirasaka-er@users.noreply.github.com>
2024-04-23 09:36:55 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+# 参考サイトのソースをそのまま貼り付け
+# https://qiita.com/std-flower/items/de9590b00c3efc928dc4
+
+from flask import Flask
+
+#Flaskオブジェクトを生成、[__name__]はおまじない的なやつ
+app = Flask(__name__)
+
+#ブラウザで[localhost]の仮想ディレクトリルートが指定された場合の挙動（hello worldを出力）
+@app.route('/')
+def hello():
+    return 'hello world'
+
+#webサーバを起動
+if __name__ == '__main__':
+    app.run(debug='true',host='0.0.0.0',port='8888')
